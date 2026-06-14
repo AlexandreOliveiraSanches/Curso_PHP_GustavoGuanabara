c1e54696ec0042849af1826904f281c10314af25
Alexandre <ao.sanches.ao@gmail.com>
2026-06-14 19:37:01 -0300

    'Ola, Mundo' em php

---
+++ olamundo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá Mundo PHP</title>
+    <style>
+        h2{
+            color: purple;
+            text-shadow: 1px 1px 1px black;
+        }
+        span{
+            color: #7B7FB5;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2>\"Olá, Mundo\" em <span>PHP</span></h2>";
+    ?>
+</body>
+</html>
