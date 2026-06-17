e97be421dbd8c54dd7baa0c7aaf37b66922adcba
Alexandre <ao.sanches.ao@gmail.com>
2026-06-17 00:11:41 -0300

    Pŕe-decremento para mostrar ano anterior

--- aula06.php
+++ aula06.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desconto para Preço</title>
+    <title>Operadores de Atribuição</title>
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
@@ -14,6 +14,8 @@
                 echo "O valor do produto é: R$" .number_format($preço,2,",",".");
                 $preço -= (($preço*10)/100);
                 echo "</br>Com desconto, ele passa a ser: R$" .number_format($preço,2,",",".");
+                $anoAtual = date('Y');
+                echo "</br>O ano atual é $anoAtual e o ano anterior é " . --$anoAtual;
             ?>
         </div>
     </div>
