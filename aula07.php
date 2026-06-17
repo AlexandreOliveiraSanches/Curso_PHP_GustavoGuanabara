24edc591bcf17036a3ac2c227f3d9343316ae233
Alexandre <ao.sanches.ao@gmail.com>
2026-06-17 18:05:20 -0300

    Operadores Relacionais, lógicos e unários

---
+++ aula07.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Relacionais</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+    <div class="terminal">
+        <div class="conteudo">
+            <?php
+                $n1 = 2;
+                $n2 = 3;
+                $op = "s";
+                $r = $op == "s" ? $n1+$n2 : $n1*$n2;
+                echo "Os números são: $n1 e $n2";
+                echo "</br>O resultado é: $r";
+                
+                $a = 3;
+                $b = "3";
+                $re = $a == $b ? "SIM" : "NÃO";
+                $re2 = $a === $b ? "SIM" : "NÃO";
+                echo "</br></br>Temos A e B. São iguais? $re";
+                echo "</br>São idênticos? $re2";
+
+                $nota1 = 10;
+                $nota2 = 7;
+                $nota3 = 3;
+                $m = ($nota1+$nota2+$nota3)/3;
+                echo "</br></br>Suas notas foram $nota1, $nota2 e $nota3. Tendo uma média de " . number_format($m,1,".");
+                echo "</br>Você foi " . ($m < 7 ? "REPROVADO." : "APROVADO.");
+
+                $idade = date("Y") - 2005;
+                echo "</br></br>Você tem $idade anos. Seu voto é " . ($idade >= 18 && $idade <= 64 ? "OBRIGATÓRIO." : "NÃO OBRIGATÓRIO.");
+            ?>
+        </div>
+    </div>
+</body>
+</html>
