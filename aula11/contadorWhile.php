9d1670c62859c827c8dab446095bde5492ef2a2a
Alexandre <ao.sanches.ao@gmail.com>
2026-08-11 13:27:19 -0300

    Criação aula11

---
+++ aula11/contadorWhile.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador com While</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    <div class="terminal">
+        <div class="conteudo">
+            <a href="index.php">Voltar</a>
+            <br/>
+            <form method="get" action="">
+ 
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ aula11/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula011</title>
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+    <div class="terminal">
+        <div class="conteudo">
+            <h1>Aula011</h1>
+
+            <ul>
+
+                <?php
+
+                    $arquivos = scandir('.');
+
+                    foreach ($arquivos as $arquivo){
+
+                        if (
+                            $arquivo != '.' &&
+                            $arquivo != 'index.php'
+                        ){
+
+                            echo "<li><a href='$arquivo'>$arquivo</a></li>";
+                        }
+                    }
+
+                ?>
+
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
